96ab23960bf7c5444cbd88f18c90e53c1af23293
Dmitry <d1maxa@yandex.ru>
2020-07-20 22:18:40 +0300

    build fix for new effects api

--- src/effects.rs
+++ src/effects.rs
@@ -1,54 +1,56 @@
+use rand::Rng;
+use rg3d::scene::particle_system::{
+    BaseEmitter, BaseEmitterBuilder, Emitter, SphereEmitterBuilder,
+};
 use rg3d::{
     core::{
-        color_gradient::{GradientPoint, ColorGradient},
         color::Color,
-        numeric_range::NumericRange,
+        color_gradient::{ColorGradient, GradientPoint},
         math::vec3::Vec3,
-        visitor::{VisitResult, Visitor, Visit}
+        numeric_range::NumericRange,
+        visitor::{Visit, VisitResult, Visitor},
     },
     engine::resource_manager::ResourceManager,
-    scene::{
-        particle_system::{
-            ParticleSystemBuilder,
-            EmitterKind,
-            EmitterBuilder,
-            SphereEmitter,
-            CustomEmitter,
-            Emit,
-            ParticleSystem,
-            Particle,
-            Emitter,
-            CustomEmitterFactory
-        },
-        node::Node,
-        transform::TransformBuilder,
-        graph::Graph,
-        base::BaseBuilder,
-    },
     resource::texture::TextureKind,
+    scene::{
+        base::BaseBuilder,
+        graph::Graph,
+        node::Node,
+        particle_system::{
+            CustomEmitter, CustomEmitterFactory, Emit, Particle, ParticleSystem,
+            ParticleSystemBuilder, SphereEmitter,
+        },
+        transform::TransformBuilder,
+    },
 };
+use std::ops::{Deref, DerefMut};
 use std::path::Path;
-use rand::Rng;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub enum EffectKind {
     BulletImpact,
     ItemAppear,
     Smoke,
-    Steam
+    Steam,
 }
 
-pub fn create(kind: EffectKind, graph: &mut Graph, resource_manager: &mut ResourceManager, pos: Vec3) {
+pub fn create(
+    kind: EffectKind,
+    graph: &mut Graph,
+    resource_manager: &mut ResourceManager,
+    pos: Vec3,
+) {
     match kind {
         EffectKind::BulletImpact => create_bullet_impact(graph, resource_manager, pos),
         EffectKind::ItemAppear => create_item_appear(graph, resource_manager, pos),
         EffectKind::Smoke => create_smoke(graph, resource_manager, pos),
-        EffectKind::Steam => create_steam(graph, resource_manager, pos)
+        EffectKind::Steam => create_steam(graph, resource_manager, pos),
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct CylinderEmitter {
+    base: BaseEmitter,
     height: f32,
     radius: f32,
 }
@@ -56,6 +58,7 @@ pub struct CylinderEmitter {
 impl CylinderEmitter {
     pub fn new() -> Self {
         Self {
+            base: Default::default(),
             height: 1.0,
             radius: 0.5,
         }
@@ -64,11 +67,25 @@ impl CylinderEmitter {
 
 impl CustomEmitter for CylinderEmitter {
     fn box_clone(&self) -> Box<dyn CustomEmitter> {
-        Box::new(*self)
+        Box::new(self.clone())
     }
 
     fn get_kind(&self) -> i32 {
         0
+    }
+}
+
+impl Deref for CylinderEmitter {
+    type Target = BaseEmitter;
+
+    fn deref(&self) -> &Self::Target {
+        &self.base
+    }
+}
+
+impl DerefMut for CylinderEmitter {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.base
     }
 }
 
@@ -84,7 +101,7 @@ impl Visit for CylinderEmitter {
 }
 
 impl Emit for CylinderEmitter {
-    fn emit(&self, _emitter: &Emitter, _particle_system: &ParticleSystem, particle: &mut Particle) {
+    fn emit(&self, _particle_system: &ParticleSystem, particle: &mut Particle) {
         // Disk point picking extended in 3D - http://mathworld.wolfram.com/DiskPointPicking.html
         let scale: f32 = rand::thread_rng().gen_range(0.0, 1.0);
         let theta = rand::thread_rng().gen_range(0.0, 2.0 * std::f32::consts::PI);
@@ -98,43 +115,54 @@ impl Emit for CylinderEmitter {
 
 pub fn register_custom_emitter_factory() {
     if let Ok(mut factory) = CustomEmitterFactory::get() {
-        factory.set_callback(Box::new(|kind| {
-            match kind {
-                0 => Ok(Box::new(CylinderEmitter::new())),
-                _ => Err(String::from("invalid custom emitter kind"))
-            }
+        factory.set_callback(Box::new(|kind| match kind {
+            0 => Ok(Box::new(CylinderEmitter::new())),
+            _ => Err(String::from("invalid custom emitter kind")),
         }))
     }
 }
 
 fn create_steam(graph: &mut Graph, resource_manager: &mut ResourceManager, pos: Vec3) {
     graph.add_node(Node::ParticleSystem(
-        ParticleSystemBuilder::new(BaseBuilder::new()
-            .with_local_transform(TransformBuilder::new()
-                .with_local_position(pos)
-                .build()))
-            .with_acceleration(Vec3::new(0.0, -0.01, 0.0))
-            .with_color_over_lifetime_gradient({
-                let mut gradient = ColorGradient::new();
-                gradient.add_point(GradientPoint::new(0.00, Color::from_rgba(150, 150, 150, 0)));
-                gradient.add_point(GradientPoint::new(0.05, Color::from_rgba(150, 150, 150, 220)));
-                gradient.add_point(GradientPoint::new(0.85, Color::from_rgba(255, 255, 255, 180)));
-                gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 255, 255, 0)));
-                gradient
-            })
-            .with_emitters(vec![
-                EmitterBuilder::new(EmitterKind::Custom(Box::new(CylinderEmitter { height: 0.2, radius: 0.2 }))).build()
-            ])
-            .with_opt_texture(resource_manager.request_texture(Path::new("data/particles/smoke_04.tga"), TextureKind::R8))
-            .build()));
+        ParticleSystemBuilder::new(
+            BaseBuilder::new()
+                .with_local_transform(TransformBuilder::new().with_local_position(pos).build()),
+        )
+        .with_acceleration(Vec3::new(0.0, -0.01, 0.0))
+        .with_color_over_lifetime_gradient({
+            let mut gradient = ColorGradient::new();
+            gradient.add_point(GradientPoint::new(0.00, Color::from_rgba(150, 150, 150, 0)));
+            gradient.add_point(GradientPoint::new(
+                0.05,
+                Color::from_rgba(150, 150, 150, 220),
+            ));
+            gradient.add_point(GradientPoint::new(
+                0.85,
+                Color::from_rgba(255, 255, 255, 180),
+            ));
+            gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 255, 255, 0)));
+            gradient
+        })
+        .with_emitters(vec![Emitter::Custom(Box::new(CylinderEmitter {
+            base: BaseEmitterBuilder::new().build(),
+            height: 0.2,
+            radius: 0.2,
+        }))])
+        .with_opt_texture(
+            resource_manager
+                .request_texture(Path::new("data/particles/smoke_04.tga"), TextureKind::R8),
+        )
+        .build(),
+    ));
 }
 
 fn create_bullet_impact(graph: &mut Graph, resource_manager: &mut ResourceManager, pos: Vec3) {
-    graph.add_node(Node::ParticleSystem(ParticleSystemBuilder::new(BaseBuilder::new()
-        .with_lifetime(1.0)
-        .with_local_transform(TransformBuilder::new()
-            .with_local_position(pos)
-            .build()))
+    graph.add_node(Node::ParticleSystem(
+        ParticleSystemBuilder::new(
+            BaseBuilder::new()
+                .with_lifetime(1.0)
+                .with_local_transform(TransformBuilder::new().with_local_position(pos).build()),
+        )
         .with_acceleration(Vec3::new(0.0, -10.0, 0.0))
         .with_color_over_lifetime_gradient({
             let mut gradient = ColorGradient::new();
@@ -144,8 +172,8 @@ fn create_bullet_impact(graph: &mut Graph, resource_manager: &mut ResourceManage
             gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 60, 0, 0)));
             gradient
         })
-        .with_emitters(vec![
-            EmitterBuilder::new(EmitterKind::Sphere(SphereEmitter::new(0.01)))
+        .with_emitters(vec![SphereEmitterBuilder::new(
+            BaseEmitterBuilder::new()
                 .with_max_particles(200)
                 .with_spawn_rate(1000)
                 .with_size_modifier_range(NumericRange::new(-0.02, -0.025))
@@ -153,47 +181,65 @@ fn create_bullet_impact(graph: &mut Graph, resource_manager: &mut ResourceManage
                 .with_x_velocity_range(NumericRange::new(-0.03, 0.03))
                 .with_y_velocity_range(NumericRange::new(0.035, 0.05))
                 .with_z_velocity_range(NumericRange::new(-0.03, 0.03))
-                .resurrect_particles(false)
-                .build()
-        ])
-        .with_opt_texture(resource_manager.request_texture(Path::new("data/particles/circle_05.png"), TextureKind::R8))
-        .build()));
+                .resurrect_particles(false),
+        )
+        .with_radius(0.01)
+        .build()])
+        .with_opt_texture(
+            resource_manager
+                .request_texture(Path::new("data/particles/circle_05.png"), TextureKind::R8),
+        )
+        .build(),
+    ));
 }
 
 fn create_smoke(graph: &mut Graph, resource_manager: &mut ResourceManager, pos: Vec3) {
-    graph.add_node(Node::ParticleSystem(ParticleSystemBuilder::new(BaseBuilder::new()
-        .with_lifetime(5.0)
-        .with_local_transform(TransformBuilder::new()
-            .with_local_position(pos)
-            .build()))
+    graph.add_node(Node::ParticleSystem(
+        ParticleSystemBuilder::new(
+            BaseBuilder::new()
+                .with_lifetime(5.0)
+                .with_local_transform(TransformBuilder::new().with_local_position(pos).build()),
+        )
         .with_acceleration(Vec3::new(0.0, 0.0, 0.0))
         .with_color_over_lifetime_gradient({
             let mut gradient = ColorGradient::new();
             gradient.add_point(GradientPoint::new(0.00, Color::from_rgba(150, 150, 150, 0)));
-            gradient.add_point(GradientPoint::new(0.05, Color::from_rgba(150, 150, 150, 220)));
-            gradient.add_point(GradientPoint::new(0.85, Color::from_rgba(255, 255, 255, 180)));
+            gradient.add_point(GradientPoint::new(
+                0.05,
+                Color::from_rgba(150, 150, 150, 220),
+            ));
+            gradient.add_point(GradientPoint::new(
+                0.85,
+                Color::from_rgba(255, 255, 255, 180),
+            ));
             gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 255, 255, 0)));
             gradient
         })
-        .with_emitters(vec![
-            EmitterBuilder::new(EmitterKind::Sphere(SphereEmitter::new(0.01)))
+        .with_emitters(vec![SphereEmitterBuilder::new(
+            BaseEmitterBuilder::new()
                 .with_max_particles(100)
                 .with_spawn_rate(50)
                 .with_x_velocity_range(NumericRange::new(-0.01, 0.01))
                 .with_y_velocity_range(NumericRange::new(0.02, 0.03))
-                .with_z_velocity_range(NumericRange::new(-0.01, 0.01))
-                .build()
-        ])
-        .with_opt_texture(resource_manager.request_texture(Path::new("data/particles/smoke_04.tga"), TextureKind::R8))
-        .build()));
+                .with_z_velocity_range(NumericRange::new(-0.01, 0.01)),
+        )
+        .with_radius(0.01)
+        .build()])
+        .with_opt_texture(
+            resource_manager
+                .request_texture(Path::new("data/particles/smoke_04.tga"), TextureKind::R8),
+        )
+        .build(),
+    ));
 }
 
 fn create_item_appear(graph: &mut Graph, resource_manager: &mut ResourceManager, pos: Vec3) {
-    graph.add_node(Node::ParticleSystem(ParticleSystemBuilder::new(BaseBuilder::new()
-        .with_lifetime(1.4)
-        .with_local_transform(TransformBuilder::new()
-            .with_local_position(pos)
-            .build()))
+    graph.add_node(Node::ParticleSystem(
+        ParticleSystemBuilder::new(
+            BaseBuilder::new()
+                .with_lifetime(1.4)
+                .with_local_transform(TransformBuilder::new().with_local_position(pos).build()),
+        )
         .with_acceleration(Vec3::new(0.0, -6.0, 0.0))
         .with_color_over_lifetime_gradient({
             let mut gradient = ColorGradient::new();
@@ -203,8 +249,8 @@ fn create_item_appear(graph: &mut Graph, resource_manager: &mut ResourceManager,
             gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 60, 0, 0)));
             gradient
         })
-        .with_emitters(vec![
-            EmitterBuilder::new(EmitterKind::Sphere(SphereEmitter::new(0.01)))
+        .with_emitters(vec![SphereEmitterBuilder::new(
+            BaseEmitterBuilder::new()
                 .with_max_particles(100)
                 .with_spawn_rate(200)
                 .with_size_modifier_range(NumericRange::new(-0.012, -0.015))
@@ -212,9 +258,14 @@ fn create_item_appear(graph: &mut Graph, resource_manager: &mut ResourceManager,
                 .with_x_velocity_range(NumericRange::new(-0.02, 0.02))
                 .with_y_velocity_range(NumericRange::new(0.035, 0.05))
                 .with_z_velocity_range(NumericRange::new(-0.02, 0.02))
-                .resurrect_particles(false)
-                .build()
-        ])
-        .with_opt_texture(resource_manager.request_texture(Path::new("data/particles/star_09.png"), TextureKind::R8))
-        .build()));
+                .resurrect_particles(false),
+        )
+        .with_radius(0.01)
+        .build()])
+        .with_opt_texture(
+            resource_manager
+                .request_texture(Path::new("data/particles/star_09.png"), TextureKind::R8),
+        )
+        .build(),
+    ));
 }

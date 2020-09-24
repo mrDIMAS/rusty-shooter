51c26fe6c3bb23599bacb3b928191821dfb5c629
Jeremy Potter <git@stormdesign.us>
2020-09-23 21:50:48 -0700

    Fix warning regarding irrefutable if...let pattern

--- src/options_menu.rs
+++ src/options_menu.rs
@@ -827,26 +827,25 @@ impl OptionsMenu {
                 }
             }
             UiMessageData::CheckBox(msg) => {
-                if let CheckBoxMessage::Check(value) = msg {
-                    let value = value.unwrap_or(false);
-                    let mut control_scheme = self.control_scheme.borrow_mut();
-                    if message.destination() == self.cb_point_shadows {
-                        settings.point_shadows_enabled = value;
-                    } else if message.destination() == self.cb_spot_shadows {
-                        settings.spot_shadows_enabled = value;
-                    } else if message.destination() == self.cb_soft_spot_shadows {
-                        settings.spot_soft_shadows = value;
-                    } else if message.destination() == self.cb_soft_point_shadows {
-                        settings.point_soft_shadows = value;
-                    } else if message.destination() == self.cb_mouse_y_inverse {
-                        control_scheme.mouse_y_inverse = value;
-                    } else if message.destination() == self.cb_smooth_mouse {
-                        control_scheme.smooth_mouse = value;
-                    } else if message.destination() == self.cb_shake_camera {
-                        control_scheme.shake_camera = value;
-                    } else if message.destination() == self.cb_use_light_scatter {
-                        settings.light_scatter_enabled = value;
-                    }
+                let CheckBoxMessage::Check(value) = msg;
+                let value = value.unwrap_or(false);
+                let mut control_scheme = self.control_scheme.borrow_mut();
+                if message.destination() == self.cb_point_shadows {
+                    settings.point_shadows_enabled = value;
+                } else if message.destination() == self.cb_spot_shadows {
+                    settings.spot_shadows_enabled = value;
+                } else if message.destination() == self.cb_soft_spot_shadows {
+                    settings.spot_soft_shadows = value;
+                } else if message.destination() == self.cb_soft_point_shadows {
+                    settings.point_soft_shadows = value;
+                } else if message.destination() == self.cb_mouse_y_inverse {
+                    control_scheme.mouse_y_inverse = value;
+                } else if message.destination() == self.cb_smooth_mouse {
+                    control_scheme.smooth_mouse = value;
+                } else if message.destination() == self.cb_shake_camera {
+                    control_scheme.shake_camera = value;
+                } else if message.destination() == self.cb_use_light_scatter {
+                    settings.light_scatter_enabled = value;
                 }
             }
             UiMessageData::Button(msg) => {
